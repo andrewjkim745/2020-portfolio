3e60bd8e1a7304fe6e101ed5d508688913213aac
Andrew <andrewk745@gmail.com>
2020-12-21 15:32:49 -0500

    added in dog animation

--- src/components/DogMove.js
+++ src/components/DogMove.js
@@ -1,5 +1,6 @@
 import React from 'react' 
-import Dog from '../assets/dog.svg'
+import Dog from '../assets/dogGif.gif'
+import Bone from '../assets/dogBone.png'
 
 
 
@@ -8,7 +9,15 @@ import Dog from '../assets/dog.svg'
 export const DogMove = () => {
     return (
         <section>
-        <img src={Dog}/>
+        <div class='w100 is-flex is-justify-content-space-between'>
+            <div class='w90 is-flex is-justify-content-flex-start'>
+                <img class='dog' style={{ width: 60, height: 60, position: 'absolute' }} src={Dog}/>
+            </div>
+            <div class='w10 is-flex is-justify-content-flex-end'>
+            <img style={{width: 60, height: 60 }} src={Bone}/>
+            </div>
+           
+        </div>
         </section>
     )
 }
