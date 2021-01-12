c74a9d865a6d01e78074808220eebef3880c4e53
Andrew <andrewk745@gmail.com>
2021-01-12 10:19:33 -0500

    created sdData file

---
+++ src/assets/sdData.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import LaptopChromebookIcon from '@material-ui/icons/LaptopChromebook';
+import AssignmentIndIcon from '@material-ui/icons/AssignmentInd';
+import ContactPhoneIcon from '@material-ui/icons/ContactPhone';
+
+
+
+
+
+
+export const sdData = [
+{
+    image: AccountCircleIcon,
+    name: 'About'
+},
+{
+    image: LaptopChromebookIcon,
+    name: 'Portfolio'
+},
+{
+    image: AssignmentIndIcon,
+    name: 'Resume'
+},
+{
+    image: ContactPhoneIcon,
+    name: 'Contact'
+}
+]
