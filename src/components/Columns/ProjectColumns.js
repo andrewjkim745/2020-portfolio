59cc235e20d46a68e8921d3f7e78d3d6684f7028
Andrew <andrewk745@gmail.com>
2021-01-14 22:54:38 -0500

    project columns update

--- src/components/Columns/ProjectColumns.js
+++ src/components/Columns/ProjectColumns.js
@@ -6,16 +6,25 @@ import projectArray from '../../assets/projectData'
 export default function ProjectColumns() {
 
 
-
+    const [ hovered, setHovered ] = useState(false) 
 
     const Projects = () => {
         return (
             <>
             {projectArray.map(project => {
                 return (
-                    <div class='column is-one-third'>
+                    <div onMouseEnter={()=> setHovered(true)} onMouseLeave={()=> setHovered(false)} class='column is-one-third'>
                         <figure class='image is-square'>
                             <img src={project.image}/>
+                            <div class='columns'>
+                            {project.languages.map(language => {
+                                return (
+                                    <div class='column'>
+                                    <img src={language}/>
+                                    </div>
+                                )
+                            })}
+                            </div>
                         </figure>
                     </div>
                 )
