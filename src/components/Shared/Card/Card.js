cb1846f1c7a3f9296271a265774665aed136c9e5
Andrew <andrewk745@gmail.com>
2021-01-19 15:52:42 -0500

    got image overlay to work correctly with css

--- src/components/Shared/Card/Card.js
+++ src/components/Shared/Card/Card.js
@@ -7,13 +7,12 @@ import React from 'react'
 
 export const Card = (props) => {
     return (
-        <div className='column is-one-third' onMouseEnter={props.onMouseEnter} onMouseLeave={props.onMouseLeave}>
+        <div style={{ position: 'relative' }} className='column is-one-third' onMouseEnter={props.onMouseEnter} onMouseLeave={props.onMouseLeave}>
             <figure class='image is-square'>
                 <img src={props.image} />
             </figure>
             {props.children}
             {props.renderVotesRatings}
-
         </div>
     )
 }
