c8f866a14c3666c5bbba51d3da04ac9d80fc4f5e
Andrew <andrewk745@gmail.com>
2021-01-20 14:44:55 -0500

    installed react spring and fixed some css

--- src/components/Shared/Card/CardItem.js
+++ src/components/Shared/Card/CardItem.js
@@ -9,14 +9,18 @@ export default class CardItem extends React.Component {
         hover: false
       };
     }
+
+    
   
     renderDescription = () => {
       if (this.state.hover) {
         return (
           <div className='description wh100 is-flex is-flex-direction-column'>
               <p>{this.props.description}</p>
-              <div class='is-flex is-justify-content-center is-align-items-center'>
+              <div class='is-flex  is-justify-content-center is-align-items-center wh100'>
+              <div class={this.state.hover ? 'lightOpacity hoverCard' : 'lightOpacityHide'}>
                   {this.props.children}
+                </div>
               </div>
           </div>
         );
