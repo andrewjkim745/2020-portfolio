e97ca1829d5a5c1792581b628577efb86b8e4be6
Andrew <andrewk745@gmail.com>
2021-01-20 14:58:26 -0500

    got react spring to work

--- src/components/Shared/Card/CardItem.js
+++ src/components/Shared/Card/CardItem.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Card } from './Card'
+import { Spring } from 'react-spring/renderprops';
 
 
 export default class CardItem extends React.Component {
@@ -18,9 +19,18 @@ export default class CardItem extends React.Component {
           <div className='description wh100 is-flex is-flex-direction-column'>
               <p>{this.props.description}</p>
               <div class='is-flex  is-justify-content-center is-align-items-center wh100'>
-              <div class={this.state.hover ? 'lightOpacity hoverCard' : 'lightOpacityHide'}>
+            <Spring
+            from={{opacity: 0}}
+            to={{opacity: .7}}
+            >
+            {props => (
+                <div class='springContainer' style={props}>
+                    <div class='hoverCard'>
                   {this.props.children}
                 </div>
+                </div>
+            )}
+            </Spring>
               </div>
           </div>
         );
