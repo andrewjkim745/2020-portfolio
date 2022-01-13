6b97e2a62a37e649d8ea8d471ed12096d387f35e
Andrew Kim <andrewk745@gmail.com>
2022-01-12 21:21:48 -0500

    added project dates into project data

--- src/assets/projectData.js
+++ src/assets/projectData.js
@@ -10,13 +10,15 @@ export const projectArray = [
         title: 'MERN-Exercise-App',
         description: 'Created a MERN Fullstack Application with authentication in a short amount of time',
         repo: 'https://github.com/andrewjkim745/MERN-Excercise-App',
-        image: 'https://i.imgur.com/vwCxceR.gif'
+        image: 'https://i.imgur.com/vwCxceR.gif',
+        date: 'Wed Dec 22 2021 15:39:36',
     },
     {
         title: 'ToDoList',
         description: 'A Full Stack ToDo CRUD application and works with full authentication',
         repo: 'https://github.com/andrewjkim745/ToDoList',
         image: 'https://media.giphy.com/media/VuLdmIZ5wi6MPRv6ur/giphy.gif',
+        date: 'Fri Dec 4 2020 12:30:31',
         languages: [
             {
                 language: Js
@@ -37,6 +39,7 @@ export const projectArray = [
         description: 'Web App designed for WhichDayz and implemented firebase authentication successfully',
         repo: 'https://github.com/whichdayz/whichdayz-web',
         image: 'https://media.giphy.com/media/Nc58PcJRLPjagOlvoM/giphy.gif',
+        date: 'Fri Sep 18 2020 13:15:17',
         languages: [
             {
                 language: Js
@@ -57,6 +60,7 @@ export const projectArray = [
         description: 'Fully responsive, implements modern UI/UX, a portfolio made for a professional pianist',
         repo: 'https://github.com/andrewjkim745/Caroline-Portfolio',
         image: 'https://media.giphy.com/media/Qyajkv8vynBHpHbTCF/giphy.gif',
+        date: 'Tue Apr 21 2020 14:21:45',
         languages: [
             {
                 language: Js
@@ -77,6 +81,7 @@ export const projectArray = [
         description: 'A phone business located in Hicksville/Glen Cove uses modern css framework Bulma',
         repo: 'https://github.com/gdelv/phoneFixIt-Client',
         image: 'https://media.giphy.com/media/RISEzca0WfSb69m4se/giphy.gif',
+        date: 'Sat Mar 7 2020 17:33:31',
         languages: [
             {
                 language: Js
@@ -97,6 +102,7 @@ export const projectArray = [
         description: 'A full stack web application inspired by Reddit. Implements restful API/CRUD functionality',
         repo: 'https://github.com/andrewjkim745/Community',
         image: 'https://media.giphy.com/media/YrqMH8JJwrapudMZOS/giphy.gif',
+        date: 'Fri Jan 17 2020 15:21:30',
         languages: [
             {
                 language: Js
@@ -120,6 +126,7 @@ export const projectArray = [
         description: 'A Recipe app aimed to be quick and efficient for users,',
         repo: 'https://github.com/andrewjkim745/Community',
         image: 'https://media.giphy.com/media/J2TtO1RVadTTSvjALK/giphy.gif',
+        date: 'Sun Dec 20 2020 14:43:11',
         languages: [
             {
                 language: Js
@@ -143,6 +150,7 @@ export const projectArray = [
         description: 'A clone of the Nike Jordan Website built from scratch, built to improve my skills as a competent developer',
         repo: 'https://github.com/gdelv/jordanFrontend',
         image: 'https://media.giphy.com/media/Jt5wIjBN8UGtpUtxbv/giphy.gif',
+        date: 'Fri Feb 14 2020 11:13:31',
         languages: [
             {
                 language: Js
@@ -160,6 +168,7 @@ export const projectArray = [
         description: 'Full stack application based on the design of Letterboxd in which users can save their favorite retro gba games.',
         repo: 'https://github.com/andrewjkim745/Gameboy-Box',
         image: 'https://media.giphy.com/media/j5z15zrcre25h4Vdyt/giphy.gif',
+        date: 'Wed Mar 4 2020 10:45:33',
         languages: [
             {
                 language: Js
@@ -184,6 +193,7 @@ export const projectArray = [
         repo: 'https://github.com/gdelv/fusionWireless',
         site: 'fusionwireless.netlify.com',
         image: 'https://media.giphy.com/media/ZFWUU9cRnPVhaiNW1d/giphy.gif',
+        date: 'Tue Feb 11 2020, 15:43:11',
         languages: [
             {
                 language: Js
@@ -204,6 +214,7 @@ export const projectArray = [
         description: 'My second Full Stack web app that implements fully functional CRUD and user Authentication',
         repo: 'https://github.com/andrewjkim745/mygamelist_frontend',
         image: 'https://media.giphy.com/media/h4UAO9SBns34CFU2wb/giphy.gif',
+        date: 'Mon Feb 3 2020 10:41:22',
         languages: [
             {
                 language: Js
@@ -227,6 +238,7 @@ export const projectArray = [
         description: 'My second project,A movie application using a MockApi and a third party API (themoviedb.org)',
         repo: 'https://github.com/andrewjkim745/Movie-Application',
         image: 'https://media.giphy.com/media/KDami0r5MKZ6mAOEw0/giphy.gif',
+        date: 'Mon Nov 18 2019 11:31:33',
         languages: [
             {
                 language: Js
