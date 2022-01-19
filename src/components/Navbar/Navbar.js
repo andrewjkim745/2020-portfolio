dd146b404b983337b0d1973f1cc4f5ca5b16254b
Andrew Kim <andrewk745@gmail.com>
2022-01-19 13:37:35 -0500

    changed some styling

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,50 +1,34 @@
 import React, { useState } from 'react'
-import './Navbar.css'
-import { Modal } from '../Shared/modal'
 import { NavLink } from 'react-router-dom'
 import SideDrawer from '../SideDrawer/SideDrawer'
-import SideBackdrop  from '../SideDrawer/SideBackdrop'
+import SideBackdrop from '../SideDrawer/SideBackdrop'
 
 
-export default function Navbar () {
-    const [ hovered, setHovered ] = useState(false)
-    const [ modal, setModal ] = useState(false)
-    const [ changed , setColor ] = useState(false)
-    
-
-    function toggleHover() {
-        setHovered(true)
-        setTimeout(() => {
-            setHovered(false)
-        }, 500)
-    }
-
-
-
+export default function Navbar() {
+    const [modal, setModal] = useState(false)
 
     const backdrop = () => {
         if (modal) {
             return (
-                <SideBackdrop close={()=>setModal(false)}/>
+                <SideBackdrop close={() => setModal(false)} />
             )
         }
     }
 
-
     return (
 
-        <div style={{position:'fixed'}}class='navbar navTransparent' role="navigation" aria-label="main navigation">
+        <div style={{ position: 'fixed' }} class='navbar navTransparent' role="navigation" aria-label="main navigation">
             <div class='navbar-brand'>
                 <NavLink exact to='/'>
                 </NavLink>
-                <div  class='navbar-item'>
+                <div class='navbar-item'>
                 </div>
-                <a onClick={() => setModal(true)} role="button" style={{display:'unset'}}class="navbar-burger has-text-primary" aria-label="menu" aria-expanded="false">
+                <a onClick={() => setModal(true)} role="button" style={{ display: 'unset' }} class="is-size-1 navbar-burger has-text-primary" aria-label="menu" aria-expanded="false">
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
                     <span aria-hidden="true"></span>
                 </a>
-                <SideDrawer onClick={()=>setModal(false)} modal={modal}/>
+                <SideDrawer onClick={() => setModal(false)} modal={modal} />
                 {backdrop()}
             </div>
         </div>
