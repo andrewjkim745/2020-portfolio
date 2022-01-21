16037cd8fa68bc7b99dfdbb6fe11246ad491ac71
Andrew Kim <andrewk745@gmail.com>
2022-01-21 16:47:49 -0500

    removed comments

--- src/components/DogMove.js
+++ src/components/DogMove.js
@@ -15,16 +15,6 @@ export default function DogMove() {
 
     const [eating, setEating] = useState(false)
 
-
-
-
-    // useEffect = () => {
-    //     window.addEventListener('scroll', handleScroll())
-    // },[]}
-
-    // handleScroll = (e) => {
-    //     if (window.pageYoffset)
-    // }
     const timeout = () => {
         setTimeout(() => {
             setEating(true)
@@ -67,22 +57,9 @@ export default function DogMove() {
     }
 
     return (
-        // setTimeout(() => {
-        //     // render inital dog and bone and then set class of both images to display none and render image of dog eating
-        // }, 5000)
         <>
         {timeout()}
         {renderDogAnimation()}
-        {/* <section>
-            <div class='w100 is-flex is-justify-content-space-between'>
-                <div class='w90 is-flex is-justify-content-flex-start'>
-                    <img class='dog' style={{ width: 60, height: 60, position: 'absolute' }} src={Dog} />
-                </div>
-                <div class='w10 is-flex is-justify-content-flex-end'>
-                    <img style={{ width: 60, height: 60 }} src={Bone} />
-                </div>
-            </div>
-        </section> */}
         </>
     )
 }
